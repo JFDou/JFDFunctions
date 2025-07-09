b7436e59c03b3d5cfbec61d68b70a7099bf271d9
JFDou <johndou@umich.edu>
2025-07-09 13:53:21 -0400

    fix so its actually works, also more configurable options

--- Code/plot_volcano.R
+++ Code/plot_volcano.R
@@ -1,37 +1,44 @@
 # function to make volcano plot of epigenome-wide association results
 #   effect estimates on x-axis and
 #   -log10 pvalues on y-axis
+#
+# sshits: dataframe of results summary of EWAS
+# est_col: column name of sshits for effect estimates
+# p_col: column name of sshits for p-values
 # xlim/ylim: x and y axis limits for plot, will auto calculate something if NULL
 # pct_include: whether to include % hyper/hypo methylated CpGs meeting pct_p_thresh
 # pct_p_thresh: p-value threshold for coloring points differently and for 
 #    considering & hyper/hypo methylated CpGs percentage
 # pct_nudge: how much to move % hyper/hypo methylated labels along x-axis further from 0
+# pct_height: what height on y-axis should percentage annotations be placed
 # title: title for plot
-# cols: three colors with positions corresponding to:
-#     1: points meeting pct_p_thresh and effect estimate > 0
-#     2: points meeting pct_p_thresh and effect estimate < 0
-#     3: points not meeting pct_p_thresh 
-plot_volcano <- function(ss.hits,xlim=NULL,ylim=NULL, 
-                         pct_include=F, pct_p_thresh=0.05, pct_nudge=0, 
+# cols: vector of three colors with positions corresponding to:
+#     1: points not meeting pct_p_thresh 
+#     2: points meeting pct_p_thresh and effect estimate > 0 
+#     3: points meeting pct_p_thresh and effect estimate < 0 
+#     if pct_include=F, then only first two slots for above/below threshold are used
+plot_volcano <- function(ss.hits, est_col="Effect.Estimate", p_col="P.Value",
+                         xlim=NULL,ylim=NULL, 
+                         pct_include=F, pct_p_thresh=0.05, 
+                         pct_nudge=0, pct_height=6, 
                          title="", 
                          cols=c('darkslategrey','cadetblue4','cadetblue3')){
   
   #get betas and pvals from single sites results
-  betas <- ss.hits$Effect.Estimate
-  pvals <- -log(ss.hits$P.Value,10)
+  betas <- ss.hits[,est_col]
+  pvals <- -log(ss.hits[,p_col],10)
   
-  #assign colors
-  pcol <- ifelse(pvals < -log(pct_p_thresh,10), cols[1], ifelse(betas>0,cols[2],cols[3]))
-  
-  #get percent hyper/hypo for sites above threshold
-  if(include_pct){
-    total <- table(pcol)[cols[2]] + table(pcol)[cols[3]]
-    pct <- c(round(table(pcol)[cols[2]]/total * 100,1),round(table(pcol)[cols[3]]/total * 100,1))
+  #assign colors and get percent hyper/hypo for sites above threshold if requested
+  if(pct_include){
+    pcol <- ifelse(pvals < -log(pct_p_thresh,10), cols[1], ifelse(betas>0,cols[2],cols[3]))
+    meet.p.tresh <- betas[pvals < -log(pct_p_thresh,10)]
+    total <- length(meet.p.tresh)
+    pct <- c(round(sum(meet.p.tresh>0)/total * 100,1),round(sum(meet.p.tresh<0)/total * 100,1))
     pct <- c(paste0(pct[1],'%'),paste0(pct[2],'%'))
   }else{
+    pcol <- ifelse(pvals < -log(pct_p_thresh,10), cols[1], cols[2])
     pct=c('','')
   }
-  
   
   #auto calculate axis range if not defined
   if(is.null(xlim)){
@@ -50,8 +57,8 @@ plot_volcano <- function(ss.hits,xlim=NULL,ylim=NULL,
     geom_hline(yintercept=7,color='red') +
     xlim(xlim) + ylim(ylim) +
     xlab(paste0('Effect Estimate')) + ylab(expression('-Log'[10]*'(P-Value)')) +
-    geom_text(aes(x=max(abs(betas))+nudge,y=6,label=pct[1],color=I(cols[2]))) + 
-    geom_text(aes(x=-max(abs(betas))-nudge,y=6,label=pct[2],color=I(cols[3]))) +
+    annotate("text", x=max(abs(betas))+pct_nudge, y=pct_height, label=pct[1], color=I(cols[2])) + 
+    annotate("text", x=-max(abs(betas))-pct_nudge, y=pct_height, label=pct[2], color=I(cols[3])) +
     theme_bw() +
     theme(axis.text=element_text(size=12),
           legend.position = "none")
